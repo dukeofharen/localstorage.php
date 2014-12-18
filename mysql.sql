3513a82da27762b5ee9809e95e7067184e03d267
Duke of Haren <info@winterwerp.it>
2014-12-18 20:44:12 +0100

    Fixed SQL file

--- mysql.sql
+++ mysql.sql
@@ -1,8 +1,14 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 CREATE TABLE IF NOT EXISTS `localstorage` (
 `id` int(11) NOT NULL,
   `key` varchar(500) NOT NULL,
   `value` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 ALTER TABLE `localstorage`
  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `localstorage`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
